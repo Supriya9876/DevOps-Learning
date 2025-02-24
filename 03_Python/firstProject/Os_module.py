3f9dc4821c6555a8c83c5fc7330f54768f59f597
Supriya9876 <137330585+Supriya9876@users.noreply.github.com>
2025-02-24 23:43:50 +0530

    Delete 03_Python/firstProject/Os_module.py

--- 03_Python/firstProject/Os_module.py
+++
@@ -1,9 +0,0 @@
-import os
-print("Current Directory",os.getcwd())
-try:
-    f=open("D:\Shell/cpuDetails.sh")
-    # print(f.read())
-    # print(f.readline())
-    print(f.readlines())
-finally:
-    f.close()
